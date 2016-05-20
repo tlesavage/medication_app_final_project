457f01c0c27fb39a8137a66d5c569f9bd31d7705
spamalope01 <spamalope01@yahoo.com>
2016-05-19 21:15:04 -0700

    started adding the medlist table and also the function to push new medicatons from the form through the constructor.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var medications = [];
 
+
 function Medication (name, prescriber, dosage, doseType, quantity, start, duration, intervals, first, second, third, withFood, beforeFood, numRefills, pharmName, pharmPhone, taking, addCurrSched, notes) {
   this.name = name;
   this.prescriber = prescriber;
@@ -18,10 +19,44 @@ function Medication (name, prescriber, dosage, doseType, quantity, start, durati
   this.pharmName = pharmName;
   this.pharmPhone = pharmPhone;
   this.taking = taking; //this is the property whose value we'll verify when determining if something is active or inactive.  clicking 'remove' on the med list is the same as clicking 'no longer taking' within the med details page.  selecting either one of those needs to set this value to 'false' (since 'true' means that the med is acive and being taken by the user).  then when we pull to populate the archive table, we just pull only meds whose this.taking = false.
+  this.pillsLeft = 0; // this has to be this.quantity - this.dosage anytime a user clicks 'taken' within the UpNextTable.
+  this.refillsLeft = 0; //this has to be numRefills - 1 every time pillsLeft === 0
   this.addCurrSched = addCurrSched;
   this.notes = notes;
   medications.push(this);
+  Medication.renderCurrTable(tbl, this);
 }
+
+Medication.renderNew = function(obj) {
+  //this is to create a new object based on the user's input into the Add New Medication fields on addmed.html
+
+  var formEl = document.getElementById('medForm');
+  formEl.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var newName = event.target.medName.value;
+    var newPrescriber = event.target.docName.value;
+
+    var newDosage = event.target.dose.value; //how do i do this with a selector?
+
+    var newQuantity = parseInt(event.target.numRx.value);
+    var newStart = event.target.startDate.value;
+    var newDuration = event.target.duration.value;
+
+    var newIntervals = event.target.duration.value; // SELECTOR
+
+    var newFirst = event.target.firstTake.value;
+    var newSecond = event.target.secondTake.value;
+    var newThird = event.target.thirdTake.value;
+    var newWithFood = event.target.withFood.value;
+    var newBeforeFood = event.target.beforeFood.value;
+    var newNumRefills = event.target.numRefills.value;
+    var newPharmName = event.target.pharmName.value;
+    var newPharmPhone = event.target.pharmNumber.value;
+    var newTaking = event.target.noLongerTaking.value;
+    var newAddCurrSched = event.target.addSched.value;
+    var newNotes = event.target.medNotes.value;
+},
+
 
 //medications array needs to be put in localStorage.
 
@@ -32,6 +67,33 @@ function Medication (name, prescriber, dosage, doseType, quantity, start, durati
 },
 
 Medication.renderCurrTable = function () {
+  if(localStorage.xxxx){
+
+  }
+
+  var medRows = document.createElement('tr');
+  var drugName = document.createElement('th');
+  drugName.textContent = medication.name;
+  drugName.setAttribute('id', medication.name);
+  medRows.appendChild(drugName);
+  for(medication in medications) {
+    if (medications[medication].taking === true) {
+      var doseData = medications[medication].dosage; //not sure if this is right...trying to pinpoint a specific property of an object within the medications array.
+      var doseTypeData = medications[medication].doseType;
+      var amtLeftData = medications.Medication. //this is going to require some math and calling from the event listener.  i have to take the medication.quantity and subtract one for each time they've said they took the medication, then i can call that and assign it to this variable.
+      var numRefillsData = medications[medication]. // this is going to have to have more math too...because i need to take the total number of refills initially provided, then know how many times the user has depleted the total number of pills (so if they were given 30, how many times did 30 = 0, then once that hits 0, subtract one from the total number of refills.)
+      medRows.appendChild(doseData);
+      medRows.appendChild(doseTypeData);
+      medRows.appendChild(amtLeftData);
+      medRows.appendChild(numRefillsData);
+    }
+    var remove = document.createElement('a');
+    var linkText = document.createTextNode("Remove");
+    a.appendChild(linkText);
+    a.title = "Remove";
+    a.href = ""; // this needs to be more functions removing the element and putting it in the archive table...so i need to see how to remove the row, but then i'll call whatever 'add to archive table' method we have.
+    medRows.appendChild(a);
+  };
   //Write some code to create table on medication.html
   //remember to assign id to Medication.name
   //Not only render/create the table but also populate it with any medication object within the medications array found in localStorage (if(localStorage.key)).  So we need to getItem and then JSON.parse the array, assign it to something, then iterate over it using a 'for' loop to populate the table.
