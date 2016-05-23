afe4ef9593e3409c4f2a72cc4ec25383ba25c78b
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-05-23 14:05:13 -0700

    Still need to compare times

--- js/index.js
+++ js/index.js
@@ -36,8 +36,9 @@ if (localStorage.drugArray) {
     tableEl.hidden = false;
 
     for (meds in medications) {
-      if (medications[meds].taking === true) {
+      if (medications[meds].taking === false) {
         var trEl = document.createElement('tr');
+        trEl.id = medications[meds].name + 'Alert'
 
         var medNameThEl = document.createElement('th');
         medNameThEl.innerHTML = '<a href="addmed.html" id=' + medications[meds].name + '>' + medications[meds].name.charAt(0).toUpperCase() + medications[meds].name.slice(1) + '</a>';
@@ -71,19 +72,23 @@ tableEl.addEventListener('click', function(event) {
   }
 });
 
-// Medication.createChart = function() {
-//   //Need chart to display on landing to show current streak
-//   //pulling also from localStorage and targeting the 'took it' or 'skipped' propery of each object, then adding 1 to the chart for each object htat has one of those selected (or deleting 1 if 'skipped')
-// })();
-
-// var schedule = {
-//   // alert: write code to compare current time to schdeule time to alert for late dose
+var schedule = {
+//
+  alert: function() {
+      //Need to research how to compare time input to current time
+      for (item in medications) {
+        // var d = new Date();
+       // var alert = Date.parse('01/01/2011 ' + d.getHours() + ':' + d.getMinutes() + ':00') > Date.parse('01/01/2011 ' + medications[0].first + ':00');
+        if(medications[item].first < currenttime) {
+          var alertRow = document.getElementById(medications[item].name + 'Alert');
+          alertRow.className = 'alert';
+          alert('You have missed your scheduled dose of ' + medications[item].name + '!');
+        }
+      }
+  },
+// pulling also from localStorage and targeting the 'took it' or 'skipped' propery of each object, then adding 1 to the chart for each object htat has one of those selected (or deleting 1 if 'skipped')
 //
 //   // taken: write code to add 1 to chart data for taken dose
 //
 //   // skipped; write code to add 1 to chart date for skipped dose
-// };
-
-//event listeners need for the following
-
-//On landing page event listener for when you click on medication name -- should take you to addmed.html page with all fields prepopulated.
+};
