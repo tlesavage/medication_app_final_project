7e883af4a2b31c3fc22ab12f2fe56128a3690475
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-05-26 20:28:00 -0700

    Mostly working

--- js/index.js
+++ js/index.js
@@ -91,26 +91,36 @@ var schedule = {
     localStorage.setItem('medClicked', jsonDrugClicked);
   },
 
-  tookEvent: function(element) {
-    // var removeTr = element.parentNode.parentNode.parentNode;
-    // tableEl.removeChild(removeTr);
-    //
-    // for(item in medications) {
-    //   if (todaysMeds[obj] === medications[item]) {
-    //     console.log(medications[item].pillsLeft + ' old');
-    //     medications[item].pillsLeft = todaysMeds[obj].pillsLeft - todaysMeds[obj].dosage;
-    //     console.log(medications[item].pillsLeft + ' new');
-    //   }
-    // }
-    // todaysMeds.splice(obj, obj); //DOESNT WANT TO REMOVE OBJECT AT INDEX 0 HALLLLP // SOMETIMES TWO OBJECTS ARE DELETED
-    // localStorage.setItem('todaysMedsStored', JSON.stringify(todaysMeds));
-    // localStorage.setItem('drugArray', JSON.stringify(medications));
+  tookEvent: function(element, todayArr, idx, medArr) {
+    var removeTr = element.parentNode.parentNode.parentNode;
+    tableEl.removeChild(removeTr);
+
+    for(item in medArr) {
+      if (todayArr[idx] === medArr[item]) {
+        console.log(medArr[item].pillsLeft + ' old');
+        medArr[item].pillsLeft = todayArr[idx].pillsLeft - todayArr[idx].dosage;
+        console.log(medArr[item].pillsLeft + ' new');
+      }
+    }
+    if(todayArr[idx] === todayArr[0]) {
+      todayArr.shift();
+    } else {
+      todayArr.splice(idx, idx); //DOESNT WANT TO REMOVE OBJECT AT INDEX 0 HALLLLP // SOMETIMES TWO OBJECTS ARE DELETED
+    }
+    localStorage.setItem('todaysMedsStored', JSON.stringify(todayArr));
+    localStorage.setItem('drugArray', JSON.stringify(medArr));
   },
 
   skipEvent: function(element) {
     var removeTr = element.parentNode.parentNode.parentNode;
     tableEl.removeChild(removeTr);
-    todaysMeds.splice(obj, obj); //DOESNT WANT TO REMOVE OBJECT AT INDEX 0 HALLLLP // SOMETIMES TWO OBJECTS ARE DELETED
+
+    if(todaysMeds[obj] === todaysMeds[0]) {
+      todaysMeds.shift();
+    } else {
+      todaysMeds.splice(obj, obj);
+    }
+
     localStorage.setItem('todaysMedsStored', JSON.stringify(todaysMeds));
   },
 
@@ -178,20 +188,7 @@ tableEl.addEventListener('click', function(event) {
     if (event.target.id === todaysMeds[obj].name) {
       schedule.clickMedName();
     } else if (event.target.value === 'took' && event.target.parentNode.parentNode.parentNode.id === todaysMeds[obj].name + 'Alert') {
-      // schedule.tookEvent(event.target);
-      var removeTr = event.target.parentNode.parentNode.parentNode;
-      tableEl.removeChild(removeTr);
-
-      for(item in medications) {
-        if (todaysMeds[obj] === medications[item]) {
-          console.log(medications[item].pillsLeft + ' old');
-          medications[item].pillsLeft = todaysMeds[obj].pillsLeft - todaysMeds[obj].dosage;
-          console.log(medications[item].pillsLeft + ' new');
-        }
-      }
-      todaysMeds.splice(obj, obj); //DOESNT WANT TO REMOVE OBJECT AT INDEX 0 HALLLLP // SOMETIMES TWO OBJECTS ARE DELETED
-      localStorage.setItem('todaysMedsStored', JSON.stringify(todaysMeds));
-      localStorage.setItem('drugArray', JSON.stringify(medications));
+      schedule.tookEvent(event.target, todaysMeds, obj, medications);
     } else if (event.target.value === 'skipped' && event.target.parentNode.parentNode.parentNode.id === todaysMeds[obj].name + 'Alert') {
       schedule.skipEvent(event.target);
     }
