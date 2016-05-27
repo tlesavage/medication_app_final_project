3782947bd048b5d7bde16fb5b6bc14da6d0203d7
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2016-05-27 13:21:04 -0700

    Linked app.js file to dry out code

--- js/addmed.js
+++ js/addmed.js
@@ -1,31 +1,8 @@
-var medications = [];
-
 if (localStorage.drugArray) {
   medications = JSON.parse(localStorage.getItem('drugArray'));
 } else {
   medications = [];
 }
-
-function Medication (name, prescriber, dosage, doseType, quantity, start, duration, intervals, first, food, numRefills, pharmName, pharmPhone, taking, addCurrSched, notes) {
-  this.name = name;
-  this.prescriber = prescriber;
-  this.dosage = dosage;
-  this.doseType = doseType;
-  this.quantity = quantity;
-  this.start = start;
-  this.duration = duration;
-  this.intervals = intervals;
-  this.first = first;
-  this.food = food;
-  this.numRefills = numRefills;
-  this.pharmName = pharmName;
-  this.pharmPhone = pharmPhone;
-  this.taking = taking;
-  this.pillsLeft = quantity;
-  this.addCurrSched = addCurrSched;
-  this.notes = notes;
-  medications.push(this);
-};
 
 var formEl = document.getElementById('medForm');
 formEl.addEventListener('submit', function(event) {
--- js/medList.js
+++ js/medList.js
@@ -1,4 +1,3 @@
-var medications = [];
 var totalListTable = document.getElementById('theTable');
 
 if (localStorage.drugArray) {
@@ -6,27 +5,6 @@ if (localStorage.drugArray) {
 } else {
   medications = [];
 }
-
-function Medication (name, prescriber, dosage, doseType, quantity, start, duration, intervals, first, food, numRefills, pharmName, pharmPhone, taking, addCurrSched, notes) {
-  this.name = name;
-  this.prescriber = prescriber;
-  this.dosage = dosage;
-  this.doseType = doseType;
-  this.quantity = quantity;
-  this.start = start;
-  this.duration = duration;
-  this.intervals = intervals;
-  this.first = first;
-  this.food = food;
-  this.numRefills = numRefills;
-  this.pharmName = pharmName;
-  this.pharmPhone = pharmPhone;
-  this.taking = taking;
-  this.pillsLeft = quantity;
-  this.addCurrSched = addCurrSched;
-  this.notes = notes;
-  medications.push(this);
-};
 
 Medication.renderCurrTable = function () {
   if(localStorage.drugArray){
